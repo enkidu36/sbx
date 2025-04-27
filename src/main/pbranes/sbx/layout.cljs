dfe65dc11d23cd27e6afadb5e0c96e774584718c
Joseph Savoie <enkidu36@yahoo.com>
2025-04-27 10:08:53 -0500

    revert test commit

--- src/main/pbranes/sbx/layout.cljs
+++ src/main/pbranes/sbx/layout.cljs
@@ -29,8 +29,7 @@
         (d/li (d/a {:href "#"} "forum"))
         (d/li (d/a {:href "#"} "login")))
       (d/ul
-        (d/li {:class "hideonmobile"} (d/
-                                       a {:href "#" :class "hideOnMobile"} "PBranes"))
+        (d/li {:class "hideonmobile"} (d/a {:href "#" :class "hideOnMobile"} "PBranes"))
         (d/li {:class "hideOnMobile"} (d/a {:href "#"} "Blog"))
         (d/li {:class "hideOnMobile"} (d/a {:href "#"} "Product"))
         (d/li {:class "hideOnMobile"} (d/a {:href "#"} "Forum"))
